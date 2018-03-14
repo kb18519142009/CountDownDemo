73fdde8f47e798263d65cbb3bca01f8a5a63e9f9
kb18519142009 <847584597@qq.com>
2018-03-14 14:29:06 +0800

    规范变量命名

--- app/src/main/java/com/example/countdowndemo/RecordCountTimer.java
+++ app/src/main/java/com/example/countdowndemo/RecordCountTimer.java
@@ -18,11 +18,11 @@ import android.widget.TextView;
 public class RecordCountTimer extends CountDownTimer {
     private static final int TIME_COUNT = 3100; //倒计时总时间为3.1s，时间防止从2s开始显示（以倒计时3s为例子）
     private static final int TIME_INTERVAL = 1000; //倒计时间隔为1s
-    private View view; //要设置动画的view
+    private View mView; //要设置动画的view
 
     private boolean mIsAnimStart;
-    private AlphaAnimation alphaAnimation; //渐变动画
-    private ScaleAnimation scaleAnimation; //缩放动画
+    private AlphaAnimation mAlphaAnimation; //渐变动画
+    private ScaleAnimation mScaleAnimation; //缩放动画
 
     private OnCountTimerListener mOnCountTimerListener;
 
@@ -53,20 +53,20 @@ public class RecordCountTimer extends CountDownTimer {
      */
     public RecordCountTimer(long millisInFuture, long countDownInterval, View view) {
         super(millisInFuture, countDownInterval);
-        this.view = view;
+        this.mView = view;
 
         // 设置透明度渐变动画
-        alphaAnimation = new AlphaAnimation(0, 1);
+        mAlphaAnimation = new AlphaAnimation(0, 1);
         // 设置动画持续时间
-        alphaAnimation.setDuration(500);
+        mAlphaAnimation.setDuration(500);
 
         // 设置缩放渐变动画
-        scaleAnimation = new ScaleAnimation(0.1f, 1f, 0.1f, 1f,
+        mScaleAnimation = new ScaleAnimation(0.1f, 1f, 0.1f, 1f,
                 Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        scaleAnimation.setDuration(500); //设置动画时长
-        scaleAnimation.setRepeatCount(1); //设置重复次数
-        scaleAnimation.setRepeatMode(Animation.REVERSE); //设置重复模式为倒转，即先从小变大再变小
-        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+        mScaleAnimation.setDuration(500); //设置动画时长
+        mScaleAnimation.setRepeatCount(1); //设置重复次数
+        mScaleAnimation.setRepeatMode(Animation.REVERSE); //设置重复模式为倒转，即先从小变大再变小
+        mScaleAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
 
@@ -97,23 +97,23 @@ public class RecordCountTimer extends CountDownTimer {
         }
 
         //每隔一秒修改一次UI
-        if (view instanceof ImageView) { // 如果是图片，在此判断，每隔一秒，改变一次图片
+        if (mView instanceof ImageView) { // 如果是图片，在此判断，每隔一秒，改变一次图片
             if (millisUntilFinished / 1000 == 1) {
-                ((ImageView) view).setImageResource(R.drawable.img_show_count_down_1);
+                ((ImageView) mView).setImageResource(R.drawable.img_show_count_down_1);
             } else if (millisUntilFinished / 1000 == 2) {
-                ((ImageView) view).setImageResource(R.drawable.img_show_count_down_2);
+                ((ImageView) mView).setImageResource(R.drawable.img_show_count_down_2);
             } else if (millisUntilFinished / 1000 == 3) {
-                ((ImageView) view).setImageResource(R.drawable.img_show_count_down_3);
+                ((ImageView) mView).setImageResource(R.drawable.img_show_count_down_3);
             }
-        } else if (view instanceof TextView) {
-            ((TextView) view).setText(String.valueOf(millisUntilFinished / 1000));
+        } else if (mView instanceof TextView) {
+            ((TextView) mView).setText(String.valueOf(millisUntilFinished / 1000));
         }
 
-        view.setVisibility(View.VISIBLE);
-        view.setEnabled(false);
+        mView.setVisibility(View.VISIBLE);
+        mView.setEnabled(false);
 
-        view.startAnimation(alphaAnimation);
-        view.startAnimation(scaleAnimation);
+        mView.startAnimation(mAlphaAnimation);
+        mView.startAnimation(mScaleAnimation);
     }
 
     @Override
